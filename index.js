08d7d25874d4ade60d50e78818389f3386bf8b8c
Superme-Abdiwahid <76221708+Superme-Abdiwahid@users.noreply.github.com>
2021-11-25 14:07:34 -0800

    Update Demo

---
+++ index.js
@@ -0,0 +1,10 @@
+'use-strict'
+/* log out the number of 5 */
+console.log(5);
+
+
+/* create a random of array of different numbers*/
+
+let random = [1,2,3,4,5,6,7,77,8,9,9,90];
+
+console.log(random);
